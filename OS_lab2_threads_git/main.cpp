6e379be38e270897c2d3f7920a449768d11c5eb0
Micha-debug <misha1310m@gmail.com>
2024-10-31 12:34:29 +0300

    Initial Commit

---
+++ OS_lab2_threads_git/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OS_lab2_threads_git
+//
+//  Created by Michael Malinovsky on 31.10.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
